a538921d0106f92a03b1cc6e23cead29baef237a
Brent Zerbe <zerbzman@gmail.com>
2015-10-13 18:01:53 -0400

    Untested
    I still can’t run asm files so I did the most that I could do. These
    are untested.

--- Ex1_ExpressionCalculation.asm
+++ Ex1_ExpressionCalculation.asm
@@ -1,8 +1,8 @@
 ; Chapter 3, Exercise 1: Expression Calculation
 
 Comment !
-Description: Using the AddTwoo program from Section 3.2 as a
-reference, write a program that calculates the following expression, 
+Description: Using the AddTwo program from Section 3.2 as a
+reference, write a program that calculates the following expression,
 using registers: A = (A + B) - (C + D). Assign integer values to the EAX,
 EBX, ECX, and EDX registers.
 !
@@ -13,8 +13,15 @@ EBX, ECX, and EDX registers.
 ExitProcess proto,dwExitCode:dword
 .code
 main PROC
+	mov	eax,1	; A = EAX
+	mov	ebx,2	; B = EBX
+	mov	ecx,3	;	C = ECX
+	mov	edx,4	; D = EDX
+	add	eax,ebx	; A + B
+	add	ecx,edx	; C + D
+	sub eax,ecx ; (A+B)-(C+D)
 
-	
+
 
 	INVOKE ExitProcess,0
 main ENDP
--- Ex2_SymbolicIntegerConstants.asm
+++ Ex2_SymbolicIntegerConstants.asm
@@ -11,10 +11,16 @@ that uses the symbols as initializers.
 .stack 4096
 ExitProcess proto,dwExitCode:dword
 
-??? = 0
+monday = 0
+tuesday = 1
+wednesday = 2
+thursday = 3
+friday = 4
+saturday = 5
+sunday = 6
 
 .data
-myDays BYTE ???
+myDays BYTE monday, tuesday, wednesday, thursday, friday, saturday, sunday
 
 .code
 main PROC
